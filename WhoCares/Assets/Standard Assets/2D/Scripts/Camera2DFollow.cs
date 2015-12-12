9508c29aca4f898d5ccdaded9c18c114497b6899
pex1l <marc.kirstein@gmail.com>
2015-12-12 22:49:06 +0100

    Camera autoscroll + slippery level objects

--- WhoCares/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ WhoCares/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace UnityStandardAssets._2D
@@ -28,24 +27,24 @@ namespace UnityStandardAssets._2D
         // Update is called once per frame
         private void Update()
         {
-            // only update lookahead pos if accelerating or changed direction
-            float xMoveDelta = (target.position - m_LastTargetPosition).x;
+            //// only update lookahead pos if accelerating or changed direction
+            //float xMoveDelta = (target.position - m_LastTargetPosition).x;
 
-            bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+            //bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
 
-            if (updateLookAheadTarget)
-            {
-                m_LookAheadPos = lookAheadFactor*Vector3.right*Mathf.Sign(xMoveDelta);
-            }
-            else
-            {
-                m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
-            }
+            //if (updateLookAheadTarget)
+            //{
+            //    m_LookAheadPos = lookAheadFactor*Vector3.right*Mathf.Sign(xMoveDelta);
+            //}
+            //else
+            //{
+            //    m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
+            //}
 
-            Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
-            Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
-
-            transform.position = newPos;
+            //Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
+            //Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
+            //newPos = new Vector3(newPos.x + 0.1f, newPos.y, newPos.z);
+            transform.position = new Vector3(transform.position.x + 0.1f, target.position.y, transform.position.z);
 
             m_LastTargetPosition = target.position;
         }
