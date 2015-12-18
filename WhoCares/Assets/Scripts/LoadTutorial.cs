934c13979f67e1dd0bd069077a29f43e319ba3f8
Niclas Kristek <nkristek@outlook.de>
2015-12-18 17:27:47 +0100

    Control-view improved by adding a new scene which fades after 3 seconds

---
+++ WhoCares/Assets/Scripts/LoadTutorial.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadTutorial : MonoBehaviour
+{
+    public bool isSingleplayer;
+    // Update is called once per frame
+    public void Update()
+    {
+        if (isSingleplayer)
+        {
+            SceneManager.LoadScene("TutorialSingleplayer");
+        } else
+        {
+            SceneManager.LoadScene("Tutorial");
+        }
+    }
+}
---
+++ WhoCares/Assets/Scripts/TutorialText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.SceneManagement;
+
+public class TutorialText : MonoBehaviour {
+
+    public bool isSingleplayer;
+    public DateTime startTime;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("ChangeLevel", 3.0f);
+    }
+	
+	// Update is called once per frame
+	void ChangeLevel () {
+	    if (isSingleplayer)
+        {
+            SceneManager.LoadScene("MainsceneSingleplayer");
+        } else
+        {
+            SceneManager.LoadScene("Mainscene");
+        }
+	}
+}
