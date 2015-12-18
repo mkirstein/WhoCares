c47153757c51dfe79d1b4f58f8c6459ba6f100c7
Niclas Kristek <nkristek@outlook.de>
2015-12-18 18:36:20 +0100

    progressbar added to the tutorial screen

---
+++ WhoCares/Assets/Scripts/TutorialLoadingBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TutorialLoadingBar : MonoBehaviour {
+
+    public Image progress;
+    public float waitTime = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        progress.fillAmount += 1.0f / waitTime * Time.deltaTime;
+    }
+}
